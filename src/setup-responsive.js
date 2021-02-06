3fc71ace90bc9b3d93c7fa98360bf2b33c1c557e
Kyle Owsen <kyle.owsen@gmail.com>
2021-02-06 13:00:15 -0800

    Disable resize observer for testing

--- src/setup-responsive.js
+++ src/setup-responsive.js
@@ -1,11 +1,11 @@
 import './styles/scale.scss';
 import './styles/scroll.scss';
 
-import ResizeObserver from 'resize-observer-polyfill';
+// import ResizeObserver from 'resize-observer-polyfill';
 
-new ResizeObserver(([event]) => {
-  document.documentElement.style.setProperty(
-    '--page-width',
-    event.contentRect.width
-  );
-}).observe(document.documentElement);
+// new ResizeObserver(([event]) => {
+//   document.documentElement.style.setProperty(
+//     '--page-width',
+//     event.contentRect.width
+//   );
+// }).observe(document.documentElement);

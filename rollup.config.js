61669077ab7afbf15bffd7fffeb353a7be573ce3
Kyle Owsen <kyle.owsen@gmail.com>
2021-02-06 13:07:45 -0800

    Disable minify

--- rollup.config.js
+++ rollup.config.js
@@ -159,7 +159,7 @@ export default {
 
     // If we're building for production (npm run build
     // instead of npm run dev), minify
-    production && terser(),
+    // production && terser(),
   ],
   watch: {
     clearScreen: false,

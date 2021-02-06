16200abbedd69be2d7a21cf1b652dc864fa5d4bc
Kyle Owsen <kyle.owsen@gmail.com>
2021-02-06 12:56:14 -0800

    Polyfill resize observer

--- src/setup-responsive.js
+++ src/setup-responsive.js
@@ -1,6 +1,8 @@
 import './styles/scale.scss';
 import './styles/scroll.scss';
 
+import ResizeObserver from 'resize-observer-polyfill';
+
 new ResizeObserver(([event]) => {
   document.documentElement.style.setProperty(
     '--page-width',

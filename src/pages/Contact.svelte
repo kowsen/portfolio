2281a4b057cb678de780f033f25e99fcb3c3d196
Kyle Owsen <me@kylebyte.com>
2021-06-10 14:41:26 -0700

    Add resume link

--- src/pages/Contact.svelte
+++ src/pages/Contact.svelte
@@ -1,3 +1,5 @@
 <span>
-  You can reach me at <a href="mailto:me@kylebyte.com">me@kylebyte.com</a>.
+  You can reach me at <a href="mailto:me@kylebyte.com">me@kylebyte.com</a>, and
+  you can read my resume
+  <a href="https://static.kylebyte.com/resume.pdf">right here</a>.
 </span>

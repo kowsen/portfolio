da0b7c65613fba4b3e962742ac2f3d80f43dc568
Kyle Owsen <me@kylebyte.com>
2022-02-11 17:58:56 -0800

    Finally update faq and add birthday

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -6,42 +6,51 @@
 
 <h3>No, like, what do you do and stuff?</h3>
 <span
-  >Oh yeah, my name's Kyle Owsen and I'm an independent game developer coming
-  off a stint as a frontend developer at Google. While I was there, I worked on
-  the core team for the Google Cloud Console for about two and a half years.
-  Before that, I built games like <a
+  >Oh yeah, my name's Kyle Owsen and I'm an independent game developer and a
+  software developer at a Seattle company called Qumulo. Before that, I was a
+  frontend developer at Google who worked on the core team for the Google Cloud
+  Console for about two and a half years. If you want to go even further back, I
+  built games like <a
     href="https://play.google.com/store/apps/details?id=com.kylebyte.mazegame"
     >Maze Frenzy</a
   >
   and <a href="https://static.kylebyte.com/apocashop">Apocashop</a> that you can
-  find on my home page, and if you want to go even further back I worked on cutting
-  edge education and whiteboarding software at SMART Technologies (as well as maintaining
-  some less than cutting edge legacy codebases)</span
+  find on my home page and I worked on cutting edge education and whiteboarding software
+  at SMART Technologies (as well as maintaining some less than cutting edge legacy
+  codebases)</span
 >
 
 <h3>What are you up to now?</h3>
 <span
-  >I'm taking some time to work on making games again. I've just finished a demo
-  for a webgame called <a href="https://static.kylebyte.com/fortune"
-    >Fortune City</a
+  >I've currently rejoined the adult world and am pretty happy as a developer at
+  Qumulo. On the side I'm still pursuing my love of game dev - I had a lot of
+  fun building <a href="https://kylebyte.itch.io/super-ball-and-chain"
+    >some games for game jams</a
   > and I'm currently playing around with new technologies and new ideas to figure
   out what I want to do next!</span
 >
 
-<h3>Oh, so you're unemployed.</h3>
-<span>Well you don't have to say it like that.</span>
-
-<h3>I didn't mean anything by it, I just thought maybe you'd lead with th—</h3>
+<h3>You're "pretty happy"?</h3>
 <span
-  >—when I said I was an independent game developer and then linked to a bunch
-  of games you've never heard of you probably should've filled in the blanks.</span
+  >Well yeah, what do you want from me? It's a good job, but a job's still just
+  a job.</span
 >
 
-<h3>Fair enough. Anything fun about you?</h3>
+<h3>I didn't mean anything by it, I just wanted to—</h3>
 <span
-  >Nothing you wouldn't expect. I'm a huge fan of retro games, Dungeons and
-  Dragons, Star Trek, baseball, and all those other things that people with
-  software engineer portfolio sites and a game dev hobby seem to like.</span
+  >—pretty good is like a ringing endorsement if you think about it. Usually
+  when people ask me how my day was my response is "not too bad," what
+  conclusion would you have jumped to for that one?</span
+>
+
+<h3>
+  Sheesh alright, never mind then. I'm glad you're "pretty happy". Anything else
+  fun about you?
+</h3>
+<span
+  >Nothing you wouldn't expect. I love games, tea, baseball, scifi, and all
+  those other things that people with software engineer portfolio sites and a
+  game dev hobby seem to like.</span
 >
 
 <style>

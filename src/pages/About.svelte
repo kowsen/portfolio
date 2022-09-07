8b980fd7d3fc919877f7ad45a5b0eee57a8b9dcc
Kyle Owsen <me@kylebyte.com>
2022-09-06 22:44:59 -0700

    Re-update faq.

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -23,34 +23,28 @@
 <h3>What are you up to now?</h3>
 <span
   >I've currently rejoined the adult world and am pretty happy as a developer at
-  Qumulo. On the side I'm still pursuing my love of game dev - I had a lot of
+  Kumospace. On the side I'm still pursuing my love of game dev - I had a lot of
   fun building <a href="https://kylebyte.itch.io/super-ball-and-chain"
     >some games for game jams</a
   > and I'm currently playing around with new technologies and new ideas to figure
   out what I want to do next!</span
 >
 
-<h3>You're "pretty happy"?</h3>
+<h3>So you're going to stick in the adult world this time?</h3>
+<span>What's that supposed to mean?</span>
+
+<h3>Nothing nothing, it's just that the last few times you've done this—</h3>
 <span
-  >Well yeah, what do you want from me? It's a good job, but a job's still just
-  a job.</span
+  >My bad, I've got to get less touchy about stuff. Yeah I've taken some time to
+  work on my own projects, but I'm thinking this'll finally be the time I can
+  find a balance between holding down a job and working on my own stuff.</span
 >
 
-<h3>I didn't mean anything by it, I just wanted to—</h3>
+<h3>I guess we'll see. Anything else fun about you?</h3>
 <span
-  >—pretty good is like a ringing endorsement if you think about it. Usually
-  when people ask me how my day was my response is "not too bad," what
-  conclusion would you have jumped to for that one?</span
->
-
-<h3>
-  Sheesh alright, never mind then. I'm glad you're "pretty happy". Anything else
-  fun about you?
-</h3>
-<span
-  >Nothing you wouldn't expect. I love games, tea, baseball, scifi, and all
-  those other things that people with software engineer portfolio sites and a
-  game dev hobby seem to like.</span
+  >Nothing you wouldn't expect. I love games, tea, baseball, and all those other
+  things that people with software engineer portfolio sites and a game dev hobby
+  seem to like.</span
 >
 
 <style>

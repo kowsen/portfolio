17bdefbdedba0b5c194d323fb5f0923d44b87356
Kyle Owsen <kyle.owsen@gmail.com>
2025-07-31 15:03:26 -0700

    INitial move to astro

--- src/pages/Contact.svelte
+++
@@ -1,5 +0,0 @@
-<span>
-  You can reach me at <a href="mailto:me@kylebyte.com">me@kylebyte.com</a>, and
-  you can read my resume
-  <a href="/resume.pdf">right here</a>.
-</span>
--- src/setup-responsive.js
+++
@@ -1,9 +0,0 @@
-import './styles/scale.scss';
-import './styles/scroll.scss';
-
-new ResizeObserver(([event]) => {
-  document.documentElement.style.setProperty(
-    '--page-width',
-    event.contentRect.width
-  );
-}).observe(document.documentElement);

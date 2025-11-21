c8df038b9d35c8d465178b968d85396399927dd6
Kyle Owsen <kyle.owsen@gmail.com>
2025-11-21 13:14:12 -0800

    Clean up allmchemy

---
+++ public/projects/allmchemy/sampledata.js
@@ -0,0 +1,34 @@
+const COMBO_MAP = {
+  "Art + Building": "Museum",
+  "Color + Skill": "Art",
+  "Light + Feeling": "Color",
+  "Practice + Talent": "Skill",
+  "Brick + Frame": "Building",
+  "Clay + Block": "Brick",
+  "Border + Picture": "Frame",
+  "Art + Block": "Design",
+  "Art + Border": "Frame",
+  "Art + Brick": "Structure",
+  // ...
+};
+
+const LEAF_EMOJI = {
+  Authority: "👑",
+  Impact: "💥",
+  Remains: "💀",
+  Strive: "🏃‍♀️",
+  Plasticity: "🧠",
+  Record: "💾",
+  // ...
+};
+
+const START_WORDS = [
+  "Light",
+  "Feeling",
+  "Practice",
+  "Talent",
+  "Clay",
+  "Block",
+  "Border",
+  "Picture",
+];
